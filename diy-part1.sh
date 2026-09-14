bfc34660b0f823e020b11d76a24bc4f4338bcf03
s;rx <8214028+wlrsx@users.noreply.github.com>
2026-09-14 16:07:48 +0800

    Update config

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,9 +14,18 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+# echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+# #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
-# Add ADGuardHome source
-git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
-chmod -R 755 ./package/luci-app-adguardhome/*
+# # Add ADGuardHome source
+# git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
+# chmod -R 755 ./package/luci-app-adguardhome/*
+
+
+# /etc/opkg/distfeeds.conf
+# src/gz immortalwrt_core https://downloads.immortalwrt.org/releases/24.10-SNAPSHOT/targets/mediatek/filogic/packages
+# src/gz immortalwrt_base https://downloads.immortalwrt.org/releases/24.10-SNAPSHOT/packages/aarch64_cortex-a53/base
+# src/gz immortalwrt_luci https://downloads.immortalwrt.org/releases/24.10-SNAPSHOT/packages/aarch64_cortex-a53/luci
+# src/gz immortalwrt_packages https://downloads.immortalwrt.org/releases/24.10-SNAPSHOT/packages/aarch64_cortex-a53/packages
+# src/gz immortalwrt_routing https://downloads.immortalwrt.org/releases/24.10-SNAPSHOT/packages/aarch64_cortex-a53/routing
+# src/gz immortalwrt_telephony https://downloads.immortalwrt.org/releases/24.10-SNAPSHOT/packages/aarch64_cortex-a53/telephony
--- diy-part5-6.6.sh
+++ diy-part5-6.6.sh
@@ -19,24 +19,24 @@
 # Modify hostname
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
 
-# 添加组播防火墙规则
-cat >> package/network/config/firewall/files/firewall.config <<EOF
-config rule
-        option name 'Allow-UDP-igmpproxy'
-        option src 'wan'
-        option dest 'lan'
-        option dest_ip '224.0.0.0/4'
-        option proto 'udp'
-        option target 'ACCEPT'        
-        option family 'ipv4'
+# # 添加组播防火墙规则
+# cat >> package/network/config/firewall/files/firewall.config <<EOF
+# config rule
+#         option name 'Allow-UDP-igmpproxy'
+#         option src 'wan'
+#         option dest 'lan'
+#         option dest_ip '224.0.0.0/4'
+#         option proto 'udp'
+#         option target 'ACCEPT'        
+#         option family 'ipv4'
 
-config rule
-        option name 'Allow-UDP-udpxy'
-        option src 'wan'
-        option dest_ip '224.0.0.0/4'
-        option proto 'udp'
-        option target 'ACCEPT'
-EOF
+# config rule
+#         option name 'Allow-UDP-udpxy'
+#         option src 'wan'
+#         option dest_ip '224.0.0.0/4'
+#         option proto 'udp'
+#         option target 'ACCEPT'
+# EOF
 
 # ===== 硬改 N60 Pro：2GB内存 + 512MB闪存(400M ubi + 100M data) DTS 补丁 =====
 python3 - <<'PYEOF'
